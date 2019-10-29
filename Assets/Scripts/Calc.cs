3dbb331cc7624bcba0c3e59f461a2b40eb0ea92c
namatthews <namatthews@tamu.edu>
2019-10-29 16:01:13 -0500

    Output text field actually works now

--- Assets/Scripts/Calc.cs
+++ Assets/Scripts/Calc.cs
@@ -1,29 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Calc : MonoBehaviour
 {
-    public Text button;
-    string answer = "";
+    public Text output;
+    string answer;
 
-    
     // Start is called before the first frame update
-    public void displayAnswer(){
-        button.text = answer;
+    void Start()
+    {
+        Debug.Log("Test");
     }
 
     public void onClick(){
-        if (button.text.Contains("DELETE"))
+        //Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        string value = EventSystem.current.currentSelectedGameObject.name;
+        if (value.Contains("DELETE"))
         {
             answer = "";
         }
         else
         {
-            answer = answer + button.text;
+            answer += value;
         }
-        //displayAnswer();
-        button.text = "yeet";
+        output.text = answer;
     }
 }
