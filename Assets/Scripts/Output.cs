e07e37aec91c0cc8c2f7557b1129ce318e5aba49
jeffreyandersen <jeffreyandersen@tamu.edu>
2019-10-29 18:08:43 -0500

    ignore

--- Assets/Scripts/Output.cs
+++ Assets/Scripts/Output.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Output : MonoBehaviour
 {
     public Text AnswerOutputText;
-    // Start is called before the first frame update
+
     void Start()
     {
         AnswerOutputText.text = "O";
