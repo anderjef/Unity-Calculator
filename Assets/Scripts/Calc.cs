74182916b87d6b190720a0aa1f1ec8bf25086f61
jeffreyandersen <jeffreyandersen@tamu.edu>
2019-10-30 16:03:02 -0500

    trying to combine with hovering

--- Assets/Scripts/Calc.cs
+++ Assets/Scripts/Calc.cs
@@ -8,39 +8,66 @@ using UnityEngine.EventSystems;
 public class Calc : MonoBehaviour
 {
     public Text output;
-    public Text firstNumVariable;
-    public Text secondNumVariable;
-    public Text rememberSecondNumVariable;
+    public Text number1;
+    public Text number2;
+    public Text rememberNumVariable;
     public Text opVariable;
     public Text rememberOpVariable;
     //public string answer = "0";
     //public string firstNum = "", secondNum = "", rememberSecondNum = ""; //the empty string represents a zero or otherwise it would be harder to determine if a second number has actually been input in the case that the user inputs a first number then hits an operator and then the enter/submit/equals button; secondNum should never be zero unless the user explicitly assigns it zero
     //public string op = "", rememberOp = ""; //operators
+    public string input;
+    public float timer = 0.0f;
+    public bool hovering = false, add = false;
 
     public void Start()
     {
         output = GameObject.Find("OutputText").GetComponentInChildren<Text>();
-        firstNumVariable= GameObject.Find("firstNumVariable").GetComponentInChildren<Text>();
-        secondNumVariable = GameObject.Find("secondNumVariable").GetComponentInChildren<Text>();
-        rememberSecondNumVariable = GameObject.Find("rememberSecondNumVariable").GetComponentInChildren<Text>();
+        number1= GameObject.Find("number1Variable").GetComponentInChildren<Text>();
+        number2 = GameObject.Find("number2Variable").GetComponentInChildren<Text>();
+        rememberNumVariable = GameObject.Find("rememberNumVariable").GetComponentInChildren<Text>();
         opVariable = GameObject.Find("opVariable").GetComponentInChildren<Text>();
         rememberOpVariable = GameObject.Find("rememberOpVariable").GetComponentInChildren<Text>();
         output.text = "0";
-        firstNumVariable.text = "";
-        secondNumVariable.text = "";
-        rememberSecondNumVariable.text = "";
+        number1.text = "";
+        number2.text = "";
+        rememberNumVariable.text = "";
         opVariable.text = "";
         rememberOpVariable.text = "";
     }
 
-    //+/- registers again after hitting an operator, second number, and ENTER
+    void Update()
+    {
+        if (hovering)
+        {
+            timer += Time.deltaTime;
+            if (timer > 2.0)
+            {
+                Debug.Log("CLICK");
+                timer = 0.0f;
+                add = true;
+                onClick();
+                add = false;
+            }
+        }
+    }
+    
     public void onClick()
     {
-        string buttonText = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+        string buttonText;
+        if (add)
+        {
+            buttonText = input;
+        }
+        else
+        {
+            buttonText = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
+        }
+        //string buttonText = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
         string answer = output.text;
-        string firstNum = firstNumVariable.text;
-        string secondNum = secondNumVariable.text;
-        string rememberSecondNum = rememberSecondNumVariable.text;
+        string firstNum = number1.text;
+        string secondNum = number2.text;
+        string rememberSecondNum = rememberNumVariable.text;
         string op = opVariable.text;
         string rememberOp = rememberOpVariable.text;
         if (buttonText == "ENTER") //could turn this into a larger switch statement but that could lead to less readability
@@ -65,6 +92,7 @@ public class Calc : MonoBehaviour
                 rememberOp = op; //in case the user wants to continue performing the same operation on the new output value
                 rememberSecondNum = secondNum; //in case the user wants to continue performing the same operation on the new output value
                 op = ""; //op gets no value after an expression is evaluated
+                secondNum = ""; //secondNum gets no value after an expression is evaluated
             }
             else //no second number input
             {
@@ -116,8 +144,9 @@ public class Calc : MonoBehaviour
                             answer = (Convert.ToDouble(firstNum) * -1).ToString();
                             break;
                     }
+                    firstNum = answer;
                     rememberOp = op;
-                    rememberSecondNum = secondNum;
+                    rememberSecondNum = firstNum; //unique instance of actually remembering the first number input instead of the second
                     op = ""; //op gets no value after an expression is evaluated
                 }
             }
@@ -127,9 +156,9 @@ public class Calc : MonoBehaviour
             answer = "0";
             firstNum = "";
             secondNum = "";
-            rememberSecondNum = ""; //should be unecessary
+            rememberSecondNum = "";
             op = "";
-            rememberOp = ""; //should be unecessary
+            rememberOp = "";
         }
         else if (buttonText == "BACKSPACE")
         {
@@ -144,6 +173,10 @@ public class Calc : MonoBehaviour
                 case "x":
                 case "/":
                 case "+/-":
+                    if (firstNum == "") //must initialize firstNum if the user didn't
+                    {
+                        firstNum = "0";
+                    }
                     if (secondNum != "") //must evaluate previous expression first; op has a value if secondNum does (and that value is not "+/-")
                     {
                         switch (op)
@@ -162,12 +195,9 @@ public class Calc : MonoBehaviour
                                 break;
                         }
                         firstNum = answer; //in case the user immediately presses a new operator
-                        secondNum = ""; //secondNum gets no value after an expression is evaluated
+                        rememberSecondNum = secondNum;
+                        rememberOp = op;
                         op = ""; //operator gets no value after an expression is evaluated (doesn't really matter as op is about to be assigned the value of the last button pressed)
-                    }
-                    if (firstNum == "") //must initialize firstNum if the user didn't
-                    {
-                        firstNum = "0";
                     }
                     if (buttonText == "+/-") //immediately resolve operation as only one input number is required
                     {
@@ -177,8 +207,6 @@ public class Calc : MonoBehaviour
                         rememberSecondNum = secondNum; //secondNum should be an empty string by this point
                         break;
                     }
-                    rememberOp = ""; //forget any old operators
-                    rememberSecondNum = ""; //forget any old second numbers
                     op = buttonText;
                     break;
                 case ".":
@@ -273,10 +301,24 @@ public class Calc : MonoBehaviour
 
         Debug.Log(answer);
         output.text = answer;
-        firstNumVariable.text = firstNum;
-        secondNumVariable.text = secondNum;
-        rememberSecondNumVariable.text = rememberSecondNum;
+        number1.text = firstNum;
+        number2.text = secondNum;
+        rememberNumVariable.text = rememberSecondNum;
         opVariable.text = op;
         rememberOpVariable.text = rememberOp;
+    }
+
+    public void MouseEnter(BaseEventData eventData)
+    {
+        input = name;
+        Debug.Log("Mouse is over GameObject " + name);
+        timer = 0.0f;
+        hovering = true;
+    }
+
+    public void MouseExit(BaseEventData eventData)
+    {
+        hovering = false;
+        Debug.Log("Mouse is exiting GameObject.");
     }
 }
